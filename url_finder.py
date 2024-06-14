2ca453a2006d9c0557fed683b005b13eed18967e
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-06-14 17:13:38 +0330

    use oop

--- url_finder.py
+++ url_finder.py
@@ -8,52 +8,59 @@ counters : counter number for while loops
 delete : delete parts of html text
 """
 
-def request(url):
-    send = requests.get(url)
-    return send.text
-search = input("search :")
+class Finder :
+    def __init__ (self) :
+        pass
+    
 
-main = request(f"https://soundcloud.com/search?q={search}")
-main = main.replace("<ul>" , "þ")
-main = main.replace("</ul>" , "Þ")
+    def finder_url(search) :
+        
+        def request(url):
+            send = requests.get(url)
+            return send.text
+        
+        main = request(f"https://soundcloud.com/search?q={search}")
+        main = main.replace("<ul>" , "þ")
+        main = main.replace("</ul>" , "Þ")
 
-counter1 = 0 
-counter2 = 0
-counter3 = 0
-while True :
-    delete1 = main[counter1]
-    if delete1 == "þ" :
-        part1 = main[:counter1]
-        main = main.replace(part1 , "")
-        main = main.replace(main[0] , "")
-        break
-    
-    counter1 = counter1 + 1
-    
-while True :
-    delete2 = main[counter2]
-    if delete2 == "Þ" :
-        part2 = main[:counter2]
-        main = main.replace(part2 , "")
-        main = main.replace(main[0] , "")
-        break
-    
-    counter2 = counter2 + 1
-    
-main = main.replace('<li><h2><a href="' , "œ")
+        counter1 = 0 
+        counter2 = 0
+        counter3 = 0
+        while True :
+            delete1 = main[counter1]
+            if delete1 == "þ" :
+                part1 = main[:counter1]
+                main = main.replace(part1 , "")
+                main = main.replace(main[0] , "")
+                break
+            
+            counter1 = counter1 + 1
+            
+        while True :
+            delete2 = main[counter2]
+            if delete2 == "Þ" :
+                part2 = main[:counter2]
+                main = main.replace(part2 , "")
+                main = main.replace(main[0] , "")
+                break
+            
+            counter2 = counter2 + 1
+            
+        main = main.replace('<li><h2><a href="' , "œ")
 
 
-while True :
-    delete3 = main[counter3]
-    if delete3 == '"':
-        part3 = main[counter3:]
-        main = main.replace(part3 , "")
-        break
-    
-    counter3 = counter3 + 1
-    
-main = main.strip()
-main = main.replace("œ" , "")
+        while True :
+            delete3 = main[counter3]
+            if delete3 == '"':
+                part3 = main[counter3:]
+                main = main.replace(part3 , "")
+                break
+            
+            counter3 = counter3 + 1
+            
+        main = main.strip()
+        main = main.replace("œ" , "")
 
-main = (f"https://soundcloud.com{main}")
-print(main)
+        main = (f"https://soundcloud.com{main}")
+
+        return main 
