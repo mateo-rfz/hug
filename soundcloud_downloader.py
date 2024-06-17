50b22cfe75ab876cb60d80254e9989d2d7ceabcb
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-06-17 04:52:33 +0330

    use oop in downloader

--- soundcloud_downloader.py
+++ soundcloud_downloader.py
@@ -1,24 +1,26 @@
 import yt_dlp
 import url_finder
 
-"""
-creat class for this file and use on main file 
-"""
-def download_soundcloud_content(content_url, download_path):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': download_path + '/%(title)s.%(ext)s',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
+class soundcloud_download:
+    def __init__(self) :
+        pass
+    def downloader(url) :
+        def download_soundcloud_content(content_url, download_path):
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': download_path + '/%(title)s.%(ext)s',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([content_url])
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([content_url])
 
-if __name__ == "__main__":
-    content_url = input("Enter the SoundCloud URL (single track or playlist): ")
-    download_path = input("Enter the path where you want to save the content: ")
-    download_soundcloud_content(content_url, download_path)
+        if __name__ == "__main__":
+            content_url = url_finder.Finder()
+            download_path = ()
+            download_soundcloud_content(content_url, download_path)
+        return 1
